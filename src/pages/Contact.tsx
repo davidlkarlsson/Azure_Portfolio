40e0bfb9cede020c6ddf26ea40cb9600a985c5a4
David Karlsson <davidfkarlsson@proton.me>
2025-09-29 13:55:28 +0200

    FIX: minor fixes to Contacts with link to GitHub and LinkedIn

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,8 +2,14 @@ export function Contact() {
   return (
     <section>
       <h2>Kontakt</h2>
-      <p>Email: david@example.com</p>
-      <p>GitHub: <a href="https://github.com/ditt-användarnamn">github.com/ditt-användarnamn</a></p>
+      <p>Email: <a href="mailto:davidfkarlsson@proton.me">davidfkarlsson@proton.me</a></p>
+      <p>GitHub: <a href="https://github.com/davidlkarlsson" target="_blank" rel="noopener noreferrer">
+        github.com/davidlkarlsson
+      </a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/david-f-karlsson/" target="_blank" rel="noopener noreferrer">
+        linkedin.com/in/david-f-karlsson/
+      </a></p>
     </section>
   );
 }
+
